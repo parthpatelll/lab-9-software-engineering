0f6fe5233208d37354bdb719e119a2fb1dfd100b
parthpatelll <168496471+parthpatelll@users.noreply.github.com>
2024-07-27 14:49:54 -0400

    Add files via upload
    Added main function

---
+++ main.py
@@ -0,0 +1,41 @@
+def encode(password):
+    encoded_password = ''
+    for char in password:
+        encoded_password += str((int(char) + 3) % 10)
+    return encoded_password
+
+
+
+def main():
+    encoded_password = ''
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        option = input("Please enter an option: ")
+
+        if option == '1':
+            password = input("Please enter the password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+
+        elif option == '2':
+            if encoded_password:
+                decoded_password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+            else:
+                print("No password has been encoded yet!")
+
+        elif option == '3':
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid option, please try again.")
+
+
+if __name__ == "__main__":
+    main()
